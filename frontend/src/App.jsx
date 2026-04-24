6ff9586b46f5c2f821b1b1ae7f0181a1da5a3f42
Oswaldo <ofdelossantosa@gmail.com>
2026-04-24 14:40:56 -0400

    chore: color, font and assets import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import heroImg from './assets/hero.png'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
-import Card from './components/Card/Card'
+// import Card from './components/Card/Card'
 import Footer from './components/Footer/Footer'
 import './App.css'
 
